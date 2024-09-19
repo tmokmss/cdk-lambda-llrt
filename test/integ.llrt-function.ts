ea85f4c2774dce6fad567a28e13bcde0cb63fc21
tmokmss <tmokmss@users.noreply.github.com>
2024-09-19 13:40:26 +0900

    ftfy

--- test/integ.llrt-function.ts
+++ test/integ.llrt-function.ts
@@ -62,6 +62,9 @@ class TestStack extends Stack {
         entry: '../example/lambda/s3.ts',
         llrtBinaryType: LlrtBinaryType.NO_SDK,
         llrtVersion: 'v0.2.2-beta',
+        bundling: {
+          nodeModules: ['@aws-sdk/client-s3'],
+        },
       });
       handler.addToRolePolicy(
         new PolicyStatement({
